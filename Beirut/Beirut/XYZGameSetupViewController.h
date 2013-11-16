485b133ecde11fc5e9af0ad60b5ccbcebd5b4e5a
Arthur Chi <arthurchi@wl-204-63.wpa.claremont.edu>
2013-11-16 02:27:13 -0800

    viewcontroller and player

---
+++ Beirut/Beirut/XYZGameSetupViewController.h
@@ -0,0 +1,13 @@
+//
+//  XYZGameSetupViewController.h
+//  Beirut
+//
+//  Created by Arthur Chi on 11/16/13.
+//  Copyright (c) 2013 Arthur Chi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZGameSetupViewController : UIViewController
+
+@end
---
+++ Beirut/Beirut/XYZGameSetupViewController.m
@@ -0,0 +1,38 @@
+//
+//  XYZGameSetupViewController.m
+//  Beirut
+//
+//  Created by Arthur Chi on 11/16/13.
+//  Copyright (c) 2013 Arthur Chi. All rights reserved.
+//
+
+#import "XYZGameSetupViewController.h"
+
+@interface XYZGameSetupViewController ()
+
+@end
+
+@implementation XYZGameSetupViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
