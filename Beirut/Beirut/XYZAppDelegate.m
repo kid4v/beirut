8c389accf81a244e41afb065cf133d9a67158c07
=George Price <gwprice@mac.com>
2013-11-15 22:16:33 -0800

    I have added a hardwood thing

--- Beirut/Beirut/XYZAppDelegate.m
+++ Beirut/Beirut/XYZAppDelegate.m
@@ -12,10 +12,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
-    [self.window makeKeyAndVisible];
+    [self.window setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"seamless-wood-texture-dark.png"]]];
     return YES;
 }
 
