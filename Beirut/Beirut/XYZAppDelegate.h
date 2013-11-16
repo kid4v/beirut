db415977b0bc948bdfbd32c2fbdb05e40909b4d0
Arthur Chi <arthurchi@wl-204-63.wpa.claremont.edu>
2013-11-15 20:37:11 -0800

    iOS

---
+++ Beirut/Beirut/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  Beirut
+//
+//  Created by Arthur Chi on 11/15/13.
+//  Copyright (c) 2013 Arthur Chi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Beirut/Beirut/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Beirut
+//
+//  Created by Arthur Chi on 11/15/13.
+//  Copyright (c) 2013 Arthur Chi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYZAppDelegate class]));
+    }
+}
